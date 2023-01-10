c92b3d18d93272aedce334e2b7f1741f69d7e7ad
anderagassi92 <115511792+anderagassi92@users.noreply.github.com>
2023-01-10 07:30:49 -0300

    dados-atletas.js

---
+++ dados.atletas.js
@@ -0,0 +1,78 @@
+class Atleta {
+  constructor(nome, idade, peso, altura, notas){
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.altura = altura;
+    this.notas = notas;
+  }
+  calculaCategoria(){
+    if(this.idade >= 9 && this.idade <= 11)
+     return "Infantil"
+    if(this.idade >= 12 && this.idade <= 13)
+     return "Juvenil"
+    if (this.idade >= 14 && this.idade <= 15)
+     return "Intermediário"
+    if (this.idade >= 16 && this.idade <= 30)
+     return "Adulto"
+       
+  }
+
+  calcularIMC(){
+    return this.peso / (this.altura * this.altura)
+  }
+  calculaMediaValida(){
+    let notasValidas = this.notas;
+    let notasMedias = notasValidas.sort(function numeros(a, b){
+      if (a === b)
+      return 0;
+      if (a < b)
+      return - 1;
+      if (a > b)
+      return 1;
+    });
+
+    let somaDasNotas = notasMedias.slice(1, 4)
+     
+  
+    let totalDasNotas = somaDasNotas.reduce(function(total, atual){
+      return total + atual
+    },0)
+
+     let media = (totalDasNotas / somaDasNotas.length).toFixed(2);
+     return media
+}
+  
+   obtemNomeAtleta(){
+    return this.nome
+   } 
+   obtemIdadeAtleta(){
+     return this.idade
+   }
+   obtemPesoAtleta(){
+     return this.peso
+   }
+   obtemNotasAtleta(){
+     return this.notas
+   }
+   obtemCategoria(){
+     return atleta.calculaCategoria()
+   }
+   obtemIMC(){
+     return atleta.calcularIMC()
+   }
+   obtemMediaValida(){
+     return atleta.calculaMediaValida()
+   }
+  }
+
+ const atleta = new Atleta("Cesar Abascal",30, 80, 1.70,
+ [10, 9.34, 8.42, 10, 7.88]);
+ 
+ console.log("Nome: " + atleta.obtemNomeAtleta());
+ console.log("Idade: " + atleta.obtemIdadeAtleta());
+ console.log("Peso: " + atleta.obtemPesoAtleta());
+ console.log("Notas: " + atleta.obtemNotasAtleta());
+ console.log("Categoria: " + atleta.obtemCategoria());
+ console.log("IMC: " + atleta.obtemIMC());
+ console.log("Média válida: " + atleta.obtemMediaValida())
